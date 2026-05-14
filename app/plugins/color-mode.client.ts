acf822903a160732dc354afcbe43d00ad36230f5
Konstantinos Zografos <konszogr@hotmail.com>
2026-05-14 19:51:25 +0300

    fix: ensure color mode is set to light in localStorage and update colorMode value

--- app/plugins/color-mode.client.ts
+++ app/plugins/color-mode.client.ts
@@ -1,4 +1,8 @@
 export default defineNuxtPlugin(() => {
+  try {
+    localStorage.setItem('nuxt-color-mode', 'light')
+  } catch {}
   const colorMode = useColorMode()
   colorMode.preference = 'light'
+  colorMode.value = 'light'
 })
